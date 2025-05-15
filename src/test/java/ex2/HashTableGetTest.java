432c32feafce00b8272adb8fba28abaf49744e25
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-15 15:03:00 +0200

    test get 1 fet

--- src/test/java/ex2/HashTableGetTest.java
+++ src/test/java/ex2/HashTableGetTest.java
@@ -1,5 +1,10 @@
 package ex2;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /*
     Get:
         S'ha de fer servir el mètode "get" per a comprovar que la taula retorna els elements correctes.
@@ -10,10 +15,37 @@ public class HashTableGetTest {
     /**
      * Obtenir un element que no col·lisiona dins una taula vuida.
      */
+    @Test
+    public void testObtenirElementNoColisioTaulaBuida() {
+        HashTable hashTable = new HashTable();
+
+        String key;
+        String msg;
+        String value;
+
+        key = "abc";
+        value = "prova";
+        hashTable.put(key, value);
+        msg = String.format(
+                "La taula no ha retornat el valor esperat per la clau: %s\nValor esperat: %s\nValor retornat: %s",
+                key, value, hashTable.get(key)
+        );
+        assertEquals(value, hashTable.get(key), msg);
+    }
 
     /**
      * Obtenir un element que col·lisiona dins una taula (1a posició dins el mateix bucket).
      */
+    @Test
+    public void testObtenirElementColisioTaula() {
+        HashTable hashTable = new HashTable();
+
+        String key;
+        String msg;
+        String value;
+
+        key 
+    }
 
     /**
      * Obtenir un element que col·lisiona dins una taula (2a posició dins el mateix bucket).
