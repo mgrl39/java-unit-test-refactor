a33a368cf83ce910304b5dbe7742f9967c4b1f76
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-15 14:35:06 +0200

    creat arxiu amb els get

---
+++ src/test/java/ex2/HashTableGetTest.java
@@ -0,0 +1,37 @@
+package ex2;
+
+/*
+    Get:
+        S'ha de fer servir el mètode "get" per a comprovar que la taula retorna els elements correctes.
+        S'ha de fer servir el mètode "put" generar les següents situacions:
+ */
+public class HashTableGetTest {
+
+    /**
+     * Obtenir un element que no col·lisiona dins una taula vuida.
+     */
+
+    /**
+     * Obtenir un element que col·lisiona dins una taula (1a posició dins el mateix bucket).
+     */
+
+    /**
+     * Obtenir un element que col·lisiona dins una taula (2a posició dins el mateix bucket).
+     */
+
+    /**
+     * Obtenir un element que col·lisiona dins una taula (3a posició dins el mateix bucket).
+     */
+
+    /**
+     * Obtenir un elements que no existeix perquè la seva posició està buida (no hi ha cap element dins el bucket).
+     */
+
+    /**
+     * Obtenir un elements que no existeix, tot i que la seva posició està ocupada per un altre que no col·lisiona.
+     */
+
+    /**
+     * Obtenir un elements que no existeix, tot i que la seva posició està ocupada per 3 elements col·lisionats.
+     */
+}
