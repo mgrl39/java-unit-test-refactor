56fdac573c1706340fc60e469c063d22ae181319
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-17 22:57:10 +0200

    afegit preguntes pel size

--- src/test/java/ex2/HashTableSizeTest.java
+++ src/test/java/ex2/HashTableSizeTest.java
@@ -7,6 +7,40 @@ package ex2;
  */
 public class HashTableSizeTest {
 
+    /**
+     * PUT:  Inserir un element que no col·lisiona dins una taula vuida (sense elements).
+     * DROP: Esborrar un element que no col·lisiona dins una taula.
+     */
+
+    /**
+     * PUT:  Inserir un element que no col·lisiona dins una taula no vuida (amb elements).
+     * DROP: Esborrar un element que si col·lisiona dins una taula (1a posició dins el mateix bucket).
+     */
+
+    /**
+     * PUT:  Inserir un element que col·lisiona dins una taula no vuida, que es col·locarà en 2a posició dins el mateix bucket.
+     * DROP: Esborrar un element que si col·lisiona dins una taula (2a posició dins el mateix bucket).
+     */
+
+    /**
+     * PUT:  Inserir un element que col·lisiona dins una taula no vuida, que es col·locarà en 3a posició dins el mateix bucket.
+     * DROP: Esborrar un element que si col·lisiona dins una taula (3a posició dins el mateix bucket).
+     */
 
 
+    /**
+     * PUT:  Inserir un elements que ja existeix (update) sobre un element que no col·lisiona dins una taula no vuida.
+     * DROP: Eliminar un elements que no existeix perquè la seva posició està buida (no hi ha cap element dins el bucket).
+     */
+
+    /**
+     * PUT:  Inserir un elements que ja existeix (update) sobre un element que si col·lisiona (1a posició) dins una taula no vuida.
+     * DROP: Eliminar un elements que no existeix, tot i que la seva posició està ocupada per un altre que no col·lisiona.
+     */
+
+    /**
+     * PUT:  Inserir un elements que ja existeix (update) sobre un element que si col·lisiona (2a posició) dins una taula no vuida.
+     * PUT:  Inserir un elements que ja existeix (update) sobre un element que si col·lisiona (3a posició) dins una taula no vuida.
+     * DROP: Eliminar un elements que no existeix, tot i que la seva posició està ocupada per 3 elements col·lisionats.
+     */
 }
